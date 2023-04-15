51be9aa3856915985c605209b47d59699d82fdf6
Felipe Mosquera <felipe298@gmail.com>
2023-04-15 13:19:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setName(name)
+  }
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input type="text" id='name' value={nombre} onChange={(e)=>{setName(e.target.value)}} />
+      <button>Enviar</button>
+    </form>
+  )
+}
+
+export default App
